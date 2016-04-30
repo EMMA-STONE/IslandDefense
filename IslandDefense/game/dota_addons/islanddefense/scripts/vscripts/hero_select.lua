93d74f291dc4417f383b09beaaa882cb17e531ea
EMMA-STONE <elifenst@gmail.com>
2016-04-30 06:40:09 -0600

    Hopefully this works?

---
+++ IslandDefense/game/dota_addons/islanddefense/scripts/vscripts/hero_select.lua
@@ -0,0 +1,6 @@
+-- File for builders to choose a specific builder after titan has been established
+
+
+-- Present pick window
+-- Once chosen, change player to that builder
+-- Only 2 allowed, afterwards cannot select
